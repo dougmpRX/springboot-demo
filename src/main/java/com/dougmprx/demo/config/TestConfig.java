3f0eab4ae822dcc0502d1f25bdbf746b44ff422c
Douglas Massote <douglasmpestana@gmail.com>
2023-02-24 09:50:31 -0300

    Payment association

--- src/main/java/com/dougmprx/demo/config/TestConfig.java
+++ src/main/java/com/dougmprx/demo/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.dougmprx.demo.entities.Category;
 import com.dougmprx.demo.entities.Order;
 import com.dougmprx.demo.entities.OrderItem;
+import com.dougmprx.demo.entities.Payment;
 import com.dougmprx.demo.entities.Product;
 import com.dougmprx.demo.entities.User;
 import com.dougmprx.demo.entities.enums.OrderStatus;
@@ -80,5 +81,10 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T22:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 }
